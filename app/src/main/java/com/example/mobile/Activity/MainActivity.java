cac0fd57944e1ad5beac743e658cbe4242331592
Dangnhuy <dangnhuy260119@gmail.com>
2023-03-20 11:10:34 +0700

    fist commit

---
+++ app/src/main/java/com/example/mobile/Activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.mobile.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mobile.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/mobile/Activity/PlayActivity.java
@@ -0,0 +1,16 @@
+package com.example.mobile.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mobile.R;
+
+public class PlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+    }
+}
